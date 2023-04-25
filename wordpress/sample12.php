7967340d649c6f8f7b3f0d6b64218d29f9e9eab0
kawashima nagisa <kawashima.it333@outlook.com>
2023-04-25 11:41:51 +0900

    Edit

--- wordpress/sample12.php
+++ wordpress/sample12.php
@@ -1,7 +1,42 @@
 <?php
-function sum(int $a, int $b): int {
+// function sum(int $a, int $b): int {
+//     return $a + $b;
+// }
+
+
+// $get_sum = function($a, $b) {
+    
+// }
+
+// $sum = sum(10, 15);
+// echo $sum;
+
+
+function sum(int $a, int $b ): int{
     return $a + $b;
 }
 
-$sum = sum(10, 15);
+
+function echo_price($callback) {
+    echo number_format($callback(1000, 500)), "円";
+}
+
+
+
+$get_sum = function ($a, $b) {
+    return $a + $b;
+};
+
+$sum = $get_sum(10, 15);
+// echo $sum;
+
+
+// echo_price($get_sum);
+echo_price(function ($a, $b)  {
+    return $a + $b;
+});
+
+$get_sum = fn($a, $b) => $a + $b;
+$sum = $get_sum(10, 15);
 echo $sum;
+
